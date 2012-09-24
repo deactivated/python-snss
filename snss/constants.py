22a1f0caaef8d4b0e48f6e10c28a08217e88f32d
mike spindel <mike@spindel.is>
2012-09-23 17:04:20 -0700

    add WindowState constants

---
+++ snss/constants.py
@@ -0,0 +1,10 @@
+# WindowShowState
+WINDOW_SHOW_STATE = [
+    0: "default",
+    1: "normal",
+    2: "minimized",
+    3: "maximized",
+    4: "inactive",              # Views only, not persisted.
+    5: "fullscreen",
+    6: "end"                    # The end of show state enum.
+]
